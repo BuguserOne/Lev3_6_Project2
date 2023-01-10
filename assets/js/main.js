45ddebe35645d7e51f5d3c0bf1cecf4cbd3ee9a2
BuguserOne <jinlingone@gmail.com>
2023-01-10 22:13:05 +0100

    Lev3_6_Project2

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/* document.write("STFU") */
+
+const toggleButton = document.getElementsByClassName("toggleButton")[0]
+
+const navBarLinks = document.getElementsByClassName("navBarLinks")[0]
+
+toggleButton.addEventListener("click", () => {
+    navBarLinks.classList.toggle("active")
+})
